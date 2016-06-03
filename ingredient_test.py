2de7faaad9e453a4d6c56834f6c342979d941e3a
Sylwia Olak <solak@8thlight.com>
2016-06-03 16:19:55 -0700

    Created ingredient class and added ingredient tests.

---
+++ ingredient_test.py
@@ -0,0 +1,18 @@
+from ingredient import Ingredient
+import unittest
+
+class IngredientTestCase(unittest.TestCase):
+    def test_ingredient_has_name(self):
+        ingredient = Ingredient("Corn", 2, 1)
+        self.assertEqual(ingredient.name, "Corn")
+
+    def test_ingredient_has_quantity(self):
+        ingredient = Ingredient("Corn", 2, 1)
+        self.assertEqual(ingredient.quantity, 2)
+
+    def test_ingredient_has_price(self):
+        ingredient = Ingredient("Corn", 2, 1)
+        self.assertEqual(ingredient.price, 1)
+
+if __name__ == '__main__':
+    unittest.main()
